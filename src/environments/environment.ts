0963c06e4e97e93467b48ccd2bdbc053a0a15cbd
Fabian Alfonso <fabian.alfons@pevaar.com>
2020-11-11 09:42:29 -0500

    Changing https for http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'https://gateway.marvel.com:443/v1/public',
+  endpoint: 'http://gateway.marvel.com:443/v1/public',
   publicKey: '12f2d21956810afe14f3b07dc210148d',
   privateKey: '11090d43f4c235794819f6e8eb92019b17762c32',
   ts: '123'
